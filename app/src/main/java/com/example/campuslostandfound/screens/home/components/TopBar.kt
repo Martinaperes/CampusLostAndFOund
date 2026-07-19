c632d38c50f93f9fe4bc661b2674ed4a2b156c67
Martina <1peresmartina@gmail.com>
2026-07-19 14:59:25 +0300

    Update: TopBar.kt

--- app/src/main/java/com/example/campuslostandfound/screens/home/components/TopBar.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/home/components/TopBar.kt
@@ -1,2 +1,24 @@
 package com.example.campuslostandfound.screens.home.components
 
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.example.campuslostandfound.ui.theme.PrimaryGreen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = "Campus Lost & Found",
+                style = MaterialTheme.typography.titleLarge,
+                color = PrimaryGreen
+            )
+        }
+    )
+
+}
