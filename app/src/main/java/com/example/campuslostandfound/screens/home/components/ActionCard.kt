c590cf653fd521c3c1e5a1a6ef96b97453057741
Martina <1peresmartina@gmail.com>
2026-07-20 20:39:42 +0300

    Update ActionCard.kt

--- app/src/main/java/com/example/campuslostandfound/screens/home/components/ActionCard.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/home/components/ActionCard.kt
@@ -1,2 +1,70 @@
 package com.example.campuslostandfound.screens.home.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import com.example.campuslostandfound.ui.theme.PrimaryGreen
+
+@Composable
+fun ActionCard(
+    title: String,
+    description: String,
+    icon: ImageVector,
+    onClick: () -> Unit
+) {
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .clickable {
+                onClick()
+            },
+
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        )
+
+    ) {
+
+        Row(
+            modifier = Modifier.padding(20.dp)
+        ) {
+
+            Icon(
+                imageVector = icon,
+                contentDescription = title,
+                tint = PrimaryGreen
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Column {
+
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleMedium
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Text(
+                    text = description,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+
+            }
+
+        }
+
+    }
+
+}
