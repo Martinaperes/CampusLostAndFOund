ecc489a3c131d64c079017a46095b85cded587c1
Martina <1peresmartina@gmail.com>
2026-07-20 20:39:48 +0300

    Update SearchBar.kt

--- app/src/main/java/com/example/campuslostandfound/screens/home/components/SearchBar.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/home/components/SearchBar.kt
@@ -1,2 +1,41 @@
 package com.example.campuslostandfound.screens.home.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SearchBar() {
+
+    var searchText by remember {
+        mutableStateOf("")
+    }
+
+    OutlinedTextField(
+        value = searchText,
+        onValueChange = {
+            searchText = it
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        placeholder = {
+            Text("Search lost or found items")
+        },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search"
+            )
+        },
+        singleLine = true
+    )
+
+}
