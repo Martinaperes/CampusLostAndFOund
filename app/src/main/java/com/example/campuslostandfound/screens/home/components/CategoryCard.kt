4f84383411e65b097cc4e80b46ec395b05190783
Martina <1peresmartina@gmail.com>
2026-07-20 20:39:44 +0300

    Update CategoryCard.kt

--- app/src/main/java/com/example/campuslostandfound/screens/home/components/CategoryCard.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/home/components/CategoryCard.kt
@@ -1,2 +1,46 @@
 package com.example.campuslostandfound.screens.home.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import com.example.campuslostandfound.ui.theme.PrimaryGreen
+
+@Composable
+fun CategoryCard(
+    title:String,
+    icon:ImageVector,
+    onClick:()->Unit
+){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.clickable{
+            onClick()
+        }
+    ){
+        Card(shape=CircleShape,
+            colors = CardDefaults.cardColors()
+        ){
+            Box(
+                modifier = Modifier
+                    .size(64.dp),
+                contentAlignment = Alignment.Center
+            ){
+                Icon(
+                    imageVector = icon,
+                    contentDescription = title,
+                    tint = PrimaryGreen
+                )
+            }
+        }
+    }
+}
