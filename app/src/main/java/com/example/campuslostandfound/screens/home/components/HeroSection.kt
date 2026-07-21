f1977427a611c56bd8957924e190629b0b48ac3b
Martina <1peresmartina@gmail.com>
2026-07-21 07:57:15 +0300

    Update HeroSection.kt

--- app/src/main/java/com/example/campuslostandfound/screens/home/components/HeroSection.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/home/components/HeroSection.kt
@@ -1,31 +1,82 @@
 package com.example.campuslostandfound.screens.home.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.WavingHand
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.example.campuslostandfound.ui.theme.PrimaryGreen
+import androidx.compose.material.icons.filled.School
 
 @Composable
-fun GreetingSection(
+fun HeroSection(
     name: String
 ) {
 
-    Column(
-        modifier = Modifier.padding(16.dp)
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+
+        shape = RoundedCornerShape(20.dp),
+
+        colors = CardDefaults.cardColors(
+            containerColor = PrimaryGreen
+        )
     ) {
 
-        Text(
-            text = "Welcome,",
-            style = MaterialTheme.typography.bodyLarge
-        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
 
-        Text(
-            text = name,
-            style = MaterialTheme.typography.headlineMedium
-        )
+            horizontalArrangement = Arrangement.SpaceBetween,
+
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Column {
+
+                Text(
+                    text = " Welcome Back,",
+                    color = Color.White,
+                    style = MaterialTheme.typography.titleMedium
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text(
+                    text = name,
+                    color = Color.White,
+                    style = MaterialTheme.typography.headlineMedium
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Text(
+                    text = "Helping TUK students reconnect\nwith their belongings.",
+                    color = Color.White.copy(alpha = 0.9f)
+                )
+
+            }
+            Icon(
+                imageVector = Icons.Default.School,
+                contentDescription = "University",
+                tint = Color.White,
+                modifier = Modifier.size(64.dp)
+            )
+
+        }
 
     }
 
