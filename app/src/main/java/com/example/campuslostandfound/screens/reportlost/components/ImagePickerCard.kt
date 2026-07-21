38c65a31bf760c90997d068012a8da267daa342b
Martina <1peresmartina@gmail.com>
2026-07-21 07:57:16 +0300

    Update ImagePickerCard.kt

--- app/src/main/java/com/example/campuslostandfound/screens/reportlost/components/ImagePickerCard.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/reportlost/components/ImagePickerCard.kt
@@ -1,2 +1,18 @@
 package com.example.campuslostandfound.screens.reportlost.components
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddAPhoto
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
