107f13d79a1be58bad57127217be7b50e770e3da
Martina <1peresmartina@gmail.com>
2026-07-21 07:57:14 +0300

    Update BottomNavItem.kt

--- app/src/main/java/com/example/campuslostandfound/navigation/BottomNavItem.kt
+++ app/src/main/java/com/example/campuslostandfound/navigation/BottomNavItem.kt
@@ -1,2 +1,8 @@
 package com.example.campuslostandfound.navigation
+import androidx.compose.ui.graphics.vector.ImageVector
 
+data class BottomNavItem(
+    val title:String,
+    val icon: ImageVector,
+    val route: String
+)
