ad1e6ca2c147b52753c274e147e97284d3c8af77
Martina <1peresmartina@gmail.com>
2026-07-21 07:57:13 +0300

    Update AppNavigation.kt

--- app/src/main/java/com/example/campuslostandfound/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/campuslostandfound/navigation/AppNavigation.kt
@@ -1,35 +1,55 @@
 package com.example.campuslostandfound.navigation
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.campuslostandfound.screens.splash.SplashScreen
 import com.example.campuslostandfound.screens.login.LoginScreen
 import com.example.campuslostandfound.screens.register.RegisterScreen
 import com.example.campuslostandfound.screens.home.HomeScreen
+import com.example.campuslostandfound.screens.home.components.BottomNavigationBar
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.padding
 @Composable
 fun AppNavigation() {
 
     val navController = rememberNavController()
+    val backStackEntry=navController.currentBackStackEntryAsState()
+    val currentRoute=backStackEntry.value?.destination?.route
 
-    NavHost(
-        navController = navController,
-        startDestination = Routes.REGISTER
-    ) {
+    Scaffold(
+        bottomBar = {
+            if(
+                currentRoute== Routes.HOME||
+                currentRoute== Routes.PROFILE
+            ){
+                BottomNavigationBar(navController)
+            }
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Routes.REGISTER,
+            modifier= Modifier.padding(innerPadding)
+        ) {
 
-        composable(Routes.SPLASH) {
-            SplashScreen()
-        }
-        composable(Routes.LOGIN) {
-            LoginScreen(navController)
-        }
-        composable(Routes.REGISTER) {
-            RegisterScreen(navController)
-        }
-        composable(Routes.HOME) {
-            HomeScreen(navController)
-        }
+            composable(Routes.SPLASH) {
+                SplashScreen()
+            }
+            composable(Routes.LOGIN) {
+                LoginScreen(navController)
+            }
+            composable(Routes.REGISTER) {
+                RegisterScreen(navController)
+            }
+            composable(Routes.HOME) {
+                HomeScreen(navController)
+            }
 
+        }
     }
 }
