78749751b4617b0b3f7e20ac12a91d26a0c67a3e
Martina <1peresmartina@gmail.com>
2026-07-23 07:33:22 +0300

    Update CustomTextField.kt

--- app/src/main/java/com/example/campuslostandfound/components/CustomTextField.kt
+++ app/src/main/java/com/example/campuslostandfound/components/CustomTextField.kt
@@ -1,2 +1,25 @@
 package com.example.campuslostandfound.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CustomTextField(
+    value:String,
+    onValueChange:(String)->Unit,
+    label:String,
+    placeholder:String,
+    modifier: Modifier= Modifier
+){
+    OutlinedTextField(
+        value=value,
+        onValueChange=onValueChange,
+        label= {
+            Text(text = label)
+        },
+        modifier = modifier.fillMaxWidth()
+    )
+}
