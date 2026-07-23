37698c6984a8730b35811d4adb1bdc5909855d42
Martina <1peresmartina@gmail.com>
2026-07-23 07:33:28 +0300

    Update ItemNameField.kt

--- app/src/main/java/com/example/campuslostandfound/screens/reportlost/components/ItemNameField.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/reportlost/components/ItemNameField.kt
@@ -1,2 +1,26 @@
 package com.example.campuslostandfound.screens.reportlost.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ItemNameField(
+    itemName: String,
+    onItemNameChange:(String)->unit
+){
+    OutlinedTextField(
+        value = itemName,
+        onValueChange = onItemNameChange,
+        label = {
+            Text("Item Name")
+        },
+        placeholder = {
+            Text("e.g Laptop")
+        },
+        modifier=Modifier.fillMaxWidth()
+    )
+
+}
