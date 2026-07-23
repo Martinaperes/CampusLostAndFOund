b20f756a67669376256b793d2a28ad06c449362f
Martina <1peresmartina@gmail.com>
2026-07-23 07:33:24 +0300

    Update ReportLostScreen.kt

--- app/src/main/java/com/example/campuslostandfound/screens/reportlost/ReportLostScreen.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/reportlost/ReportLostScreen.kt
@@ -1,2 +1,26 @@
 package com.example.campuslostandfound.screens.reportlost
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.example.campuslostandfound.screens.reportlost.components.ItemNameField
+import com.example.campuslostandfound.components.CustomTextField
+
+@Composable
+fun ReportLostScreen() {
+    var itemName by remember {
+        mutableStateOf("")
+    }
+    CustomTextField(
+        value = itemName,
+        onValueChange = {
+            itemName = it
+        },
+        label = "Item Name",
+        placeholder = "e.g Laptop"
+    )
+
+
+}
