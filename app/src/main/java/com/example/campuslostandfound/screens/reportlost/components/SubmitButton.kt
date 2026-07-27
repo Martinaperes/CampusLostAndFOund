760ba44dfc6698f53ea37921aa4c66b5e2fddce0
Martina <1peresmartina@gmail.com>
2026-07-27 08:22:31 +0300

    Update SubmitButton.kt

--- app/src/main/java/com/example/campuslostandfound/screens/reportlost/components/SubmitButton.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/reportlost/components/SubmitButton.kt
@@ -1,2 +1,19 @@
 package com.example.campuslostandfound.screens.reportlost.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SubmitButton(
+    onClick:()->Unit
+){
+    Button(
+        onClick=onClick,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text("Submit Report")
+    }
+}
