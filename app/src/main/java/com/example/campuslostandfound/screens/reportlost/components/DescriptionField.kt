b80ec910f61ae0a822513ffc25015d96afd52bf2
Martina <1peresmartina@gmail.com>
2026-07-27 08:22:29 +0300

    Update DescriptionField.kt

--- app/src/main/java/com/example/campuslostandfound/screens/reportlost/components/DescriptionField.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/reportlost/components/DescriptionField.kt
@@ -1,2 +1,27 @@
 package com.example.campuslostandfound.screens.reportlost.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DescriptionField(
+    description:String,
+    onDescriptionChange:(String)->Unit
+){
+    OutlinedTextField(
+        value = description,
+        onValueChange = onDescriptionChange,
+        label={
+            Text("Description")
+        },
+        placeholder = {
+            Text("Describe the lost item ...")
+        },
+        modifier = Modifier.fillMaxWidth(),
+        minLines=4,
+        maxLines = 6
+    )
+}
