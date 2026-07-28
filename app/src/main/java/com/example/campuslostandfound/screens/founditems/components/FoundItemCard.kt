5de428f550f0c7081a931feb3cd86f2b0c6e97af
Martina <1peresmartina@gmail.com>
2026-07-28 22:08:08 +0300

    Update FoundItemCard.kt

--- app/src/main/java/com/example/campuslostandfound/screens/founditems/components/FoundItemCard.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/founditems/components/FoundItemCard.kt
@@ -1,2 +1,36 @@
 package com.example.campuslostandfound.screens.founditems.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FoundItemCard(
+    itemName: String,
+    location: String,
+    date: String,
+    onClick: ()-> Unit
+){
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable{
+                onClick()
+            }
+    ){
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ){
+            Text(text=itemName)
+
+            Text(text=location)
+
+            Text(text=date)
+        }
+    }
+}
