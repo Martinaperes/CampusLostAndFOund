11b05746348ae8180fbc153220821e2d974b766d
Martina <1peresmartina@gmail.com>
2026-07-29 08:20:34 +0300

    Update ItemDetailsScreen.kt

--- app/src/main/java/com/example/campuslostandfound/screens/itemdetails/ItemDetailsScreen.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/itemdetails/ItemDetailsScreen.kt
@@ -1,2 +1,40 @@
 package com.example.campuslostandfound.screens.itemdetails
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun ItemDetailsScreen(){
+    LazyColumn(
+    modifier=Modifier.fillMaxSize(),
+    contentPadding= PaddingValues(16.dp)
+    ){
+        item{
+            //image
+        }
+        item{
+            //spacer
+        }
+        item{
+            //item information
+        }
+        item {
+            //spacer
+        }
+        item{
+            //description
+        }
+        item{
+            //spacer
+        }
+
+        item{
+            //claim button
+        }
+    }
+}
