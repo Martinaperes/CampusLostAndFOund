eb36cb3fa70039d5aac4e75f449cddd68366add1
Martina <1peresmartina@gmail.com>
2026-07-31 07:16:20 +0300

    Update ItemDetailsScreen.kt

--- app/src/main/java/com/example/campuslostandfound/screens/itemdetails/ItemDetailsScreen.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/itemdetails/ItemDetailsScreen.kt
@@ -1,11 +1,16 @@
 package com.example.campuslostandfound.screens.itemdetails
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.campuslostandfound.R
 
 
 @Composable
@@ -15,10 +20,21 @@ fun ItemDetailsScreen(){
     contentPadding= PaddingValues(16.dp)
     ){
         item{
-            //image
+            Card(
+                shape= RoundedCornerShape(20.dp)
+            ){
+                Image(
+                    painter=painterResource(id=R.drawable.placeholder_item),
+                    contentDescription = "Found Item",
+                    modifier=Modifier
+                        .fillMaxWidth()
+                        .height(250.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
         }
         item{
-            //spacer
+            Spacer(modifier = Modifier.height(20.dp))
         }
         item{
             //item information
