54bc32ee5f62dd677e37f6c7388a69d707ff344f
Martina <1peresmartina@gmail.com>
2026-07-31 07:16:21 +0300

    Update DetailRow.kt

--- app/src/main/java/com/example/campuslostandfound/screens/itemdetails/components/DetailRow.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/itemdetails/components/DetailRow.kt
@@ -1,2 +1,31 @@
 package com.example.campuslostandfound.screens.itemdetails.components
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DetailRow(
+    label:String,
+    value:String
+){
+    Column(
+        modifier=Modifier.fillMaxWidth()
+    ) {
+        Text(
+            text=label,
+            style= MaterialTheme.typography.labelLarge,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Text(
+            text = value,
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
