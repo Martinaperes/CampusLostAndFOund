2dae5f62e14fea02e966d4e65f1045c836b51a82
Martina <1peresmartina@gmail.com>
2026-08-04 14:18:26 +0300

    Update ReportItem.kt

--- app/src/main/java/com/example/campuslostandfound/screens/myreports/ReportItem.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/myreports/ReportItem.kt
@@ -1,2 +1,9 @@
 package com.example.campuslostandfound.screens.myreports
 
+data class ReportItem(
+    val itemName: String,
+    val type: String,
+    val location:String,
+    val date:String,
+    val status:String
+)
