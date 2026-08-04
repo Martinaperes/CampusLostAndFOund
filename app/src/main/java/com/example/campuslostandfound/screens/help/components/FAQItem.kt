ebb740e87b6589bd6fa9362111b128a43b3c4cf6
Martina <1peresmartina@gmail.com>
2026-08-04 14:33:03 +0300

    Update FAQItem.kt

--- app/src/main/java/com/example/campuslostandfound/screens/help/components/FAQItem.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/help/components/FAQItem.kt
@@ -1,2 +1,79 @@
 package com.example.campuslostandfound.screens.help.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FAQItem(
+    question: String,
+    answer: String
+) {
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(2.dp)
+    ) {
+
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        expanded = !expanded
+                    },
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Text(
+                    text = question,
+                    modifier = Modifier.weight(1f),
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Icon(
+                    imageVector = if (expanded)
+                        Icons.Default.ExpandLess
+                    else
+                        Icons.Default.ExpandMore,
+                    contentDescription = null
+                )
+
+            }
+
+            if (expanded) {
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Text(
+                    text = answer,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+
+            }
+
+        }
+
+    }
+
+}
