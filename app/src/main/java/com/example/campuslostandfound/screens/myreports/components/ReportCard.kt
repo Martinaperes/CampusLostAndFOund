839d18e6bfc6eae86126b9ff65c5e6b389b406f2
Martina <1peresmartina@gmail.com>
2026-08-04 14:18:27 +0300

    Update ReportCard.kt

--- app/src/main/java/com/example/campuslostandfound/screens/myreports/components/ReportCard.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/myreports/components/ReportCard.kt
@@ -1,2 +1,57 @@
 package com.example.campuslostandfound.screens.myreports.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.campuslostandfound.ui.theme.PrimaryGreen
+
+@Composable
+fun ReportCard(
+    itemName: String,
+    type: String,
+    location: String,
+    date: String,
+    status: String,
+    onClick: () -> Unit
+){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                onClick()
+            },
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+
+            Text(
+                text = itemName,
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            TypeChip(type = type)
+
+            Text("Location: $location")
+
+            Text("Date: $date")
+
+            StatusChip(status = status)
+
+        }
+
+    }
+
+}
