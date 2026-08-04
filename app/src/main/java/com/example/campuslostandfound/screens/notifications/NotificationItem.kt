6f8cfbbd4d7d679560713b2e5999797f56d9c1bf
Martina <1peresmartina@gmail.com>
2026-08-04 14:28:27 +0300

    Update NotificationItem.kt

--- app/src/main/java/com/example/campuslostandfound/screens/notifications/NotificationItem.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/notifications/NotificationItem.kt
@@ -1,2 +1,8 @@
 package com.example.campuslostandfound.screens.notifications
 
+data class NotificationItem(
+    val title: String,
+    val message: String,
+    val time: String,
+    val isRead: Boolean
+)
