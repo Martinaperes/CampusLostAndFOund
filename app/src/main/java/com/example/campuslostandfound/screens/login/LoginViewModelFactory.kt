6b21b2f6077dfc2de9f0f768601cc2c46b795043
Martina <1peresmartina@gmail.com>
2026-08-10 12:42:08 +0300

    Update LoginViewModelFactory.kt

--- app/src/main/java/com/example/campuslostandfound/screens/login/LoginViewModelFactory.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/login/LoginViewModelFactory.kt
@@ -1,2 +1,30 @@
 package com.example.campuslostandfound.screens.login
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.campuslostandfound.data.repository.UserRepository
+import com.example.campuslostandfound.firebase.AuthRepository
+
+class LoginViewModelFactory(
+    private val authRepository: AuthRepository,
+    private val userRepository: UserRepository
+) : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(
+        modelClass: Class<T>
+    ): T {
+
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+
+            return LoginViewModel(
+                authRepository,
+                userRepository
+            ) as T
+        }
+
+        throw IllegalArgumentException(
+            "Unknown ViewModel class"
+        )
+    }
+}
