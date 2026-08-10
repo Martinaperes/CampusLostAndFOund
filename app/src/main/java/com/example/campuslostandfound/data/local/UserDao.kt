a2cd862d3b603d1d1d243cb83f15a7adad2c49a0
Martina <1peresmartina@gmail.com>
2026-08-10 11:19:44 +0300

    Update UserDao.kt

--- app/src/main/java/com/example/campuslostandfound/data/local/UserDao.kt
+++ app/src/main/java/com/example/campuslostandfound/data/local/UserDao.kt
@@ -1,2 +1,26 @@
 package com.example.campuslostandfound.data.local
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface UserDao {
+
+    @Insert
+    suspend fun insertUser(user: UserEntity): Long
+
+    @Query("SELECT * FROM users")
+    suspend fun getAllUsers(): List<UserEntity>
+
+    @Query("SELECT * FROM users WHERE userId = :userId")
+    suspend fun getUserById(userId: Int): UserEntity?
+
+    @Update
+    suspend fun updateUser(user: UserEntity)
+
+    @Delete
+    suspend fun deleteUser(user: UserEntity)
+}
