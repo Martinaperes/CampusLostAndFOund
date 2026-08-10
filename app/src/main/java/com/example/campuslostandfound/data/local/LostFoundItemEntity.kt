5b01063243bf4c44f8a0e3eda6682e66770dba6b
Martina <1peresmartina@gmail.com>
2026-08-10 11:19:44 +0300

    Update LostFoundItemEntity.kt

--- app/src/main/java/com/example/campuslostandfound/data/local/LostFoundItemEntity.kt
+++ app/src/main/java/com/example/campuslostandfound/data/local/LostFoundItemEntity.kt
@@ -1,2 +1,42 @@
 package com.example.campuslostandfound.data.local
 
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "lost_found_items",
+    foreignKeys = [
+        ForeignKey(
+            entity = UserEntity::class,
+            parentColumns = ["userId"],
+            childColumns = ["userId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
+data class LostFoundItemEntity(
+
+    @PrimaryKey(autoGenerate = true)
+    val itemId: Int = 0,
+
+    val userId: Int,
+
+    val itemName: String,
+
+    val category: String,
+
+    val type: String,
+
+    val description: String,
+
+    val location: String,
+
+    val date: String,
+
+    val serialNumber: String?,
+
+    val imageUri: String?,
+
+    val status: String
+)
