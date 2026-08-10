6e503997d84d7eb6ca3e67333744b0729609b0ca
Martina <1peresmartina@gmail.com>
2026-08-10 12:01:10 +0300

    Update AuthRepository.kt

--- app/src/main/java/com/example/campuslostandfound/firebase/AuthRepository.kt
+++ app/src/main/java/com/example/campuslostandfound/firebase/AuthRepository.kt
@@ -1,38 +1,65 @@
 package com.example.campuslostandfound.firebase
 
 import com.google.firebase.auth.FirebaseAuth
-class AuthRepository{
-    private val auth= FirebaseAuth.getInstance()
+
+class AuthRepository {
+
+    private val auth = FirebaseAuth.getInstance()
+
     fun registerUser(
-        email:String,
-        password:String,
-        onSuccess:()->Unit,
-        onFailure:(String)->Unit
-    ){
-        auth.createUserWithEmailAndPassword(email,password)
+        email: String,
+        password: String,
+        onSuccess: () -> Unit,
+        onFailure: (String) -> Unit
+    ) {
+
+        auth.createUserWithEmailAndPassword(
+            email,
+            password
+        )
             .addOnSuccessListener {
-                onSuccess
+                onSuccess()
             }
-            .addOnFailureListener {exception ->
-                onFailure(exception.message ?: "Registration failed")
+            .addOnFailureListener { exception ->
+                onFailure(
+                    exception.message ?: "Registration failed"
+                )
             }
     }
+
     fun loginUser(
         email: String,
         password: String,
-        onSuccess:()->Unit,
-        onFailure:(String)->Unit
-    )
-    {
-        auth.signInWithEmailAndPassword(
-            email,password)
-                .addOnCompleteListener{task->
-                    if(task.isSuccessful){
-                        onSuccess()
-                    }else{
-                        onFailure(task.exception?.message?:"Login failed")
-                    }
-                }
+        onSuccess: () -> Unit,
+        onFailure: (String) -> Unit
+    ) {
 
+        auth.signInWithEmailAndPassword(
+            email,
+            password
+        )
+            .addOnSuccessListener {
+                onSuccess()
+            }
+            .addOnFailureListener { exception ->
+                onFailure(
+                    exception.message ?: "Login failed"
+                )
+            }
+    }
+
+    fun logout() {
+        auth.signOut()
+    }
+
+    fun getCurrentUser() =
+        auth.currentUser
+
+    fun getCurrentUserUid(): String? {
+        return auth.currentUser?.uid
+    }
+
+    fun isUserLoggedIn(): Boolean {
+        return auth.currentUser != null
     }
 }
