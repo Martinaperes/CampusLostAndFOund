f4235a6bdb86e4dee223bc2f055cf64b5004559a
Martina <1peresmartina@gmail.com>
2026-08-10 12:01:14 +0300

    Update ReportLostViewModelFactory.kt

--- app/src/main/java/com/example/campuslostandfound/screens/reportlost/ReportLostViewModelFactory.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/reportlost/ReportLostViewModelFactory.kt
@@ -1,2 +1,24 @@
 package com.example.campuslostandfound.screens.reportlost
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.campuslostandfound.data.repository.LostFoundRepository
+
+class ReportLostViewModelFactory(
+    private val repository: LostFoundRepository
+) : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(
+        modelClass: Class<T>
+    ): T {
+
+        if (modelClass.isAssignableFrom(ReportLostViewModel::class.java)) {
+            return ReportLostViewModel(repository) as T
+        }
+
+        throw IllegalArgumentException(
+            "Unknown ViewModel class"
+        )
+    }
+}
