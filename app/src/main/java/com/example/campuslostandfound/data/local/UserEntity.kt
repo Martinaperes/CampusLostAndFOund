6c9c14b296a0c4088fe9957a7ccd561066faab88
Martina <1peresmartina@gmail.com>
2026-08-10 11:19:44 +0300

    Update UserEntity.kt

--- app/src/main/java/com/example/campuslostandfound/data/local/UserEntity.kt
+++ app/src/main/java/com/example/campuslostandfound/data/local/UserEntity.kt
@@ -1,2 +1,21 @@
 package com.example.campuslostandfound.data.local
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class UserEntity(
+
+    @PrimaryKey(autoGenerate = true)
+    val userId: Int = 0,
+
+    val registrationNumber: String,
+
+    val name: String,
+
+    val department: String,
+
+    val phoneNumber: String,
+
+    val email: String
+)
