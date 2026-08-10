5c451c261449baaa01279c2778d4c3ec9872a40c
Martina <1peresmartina@gmail.com>
2026-08-10 12:01:06 +0300

    Update UserRepository.kt"

--- app/src/main/java/com/example/campuslostandfound/data/ repository/UserRepository.kt
+++ app/src/main/java/com/example/campuslostandfound/data/ repository/UserRepository.kt
@@ -1,2 +1,48 @@
-package com.example.campuslostandfound.data.` repository`
+package com.example.campuslostandfound.data.repository
 
+import com.example.campuslostandfound.data.local.UserDao
+import com.example.campuslostandfound.data.local.UserEntity
+
+class UserRepository(
+    private val userDao: UserDao
+) {
+
+    // CREATE
+    suspend fun createUser(
+        user: UserEntity
+    ): Long {
+        return userDao.insertUser(user)
+    }
+
+    // READ
+    suspend fun getUserById(
+        userId: Int
+    ): UserEntity? {
+        return userDao.getUserById(userId)
+    }
+
+    // READ using Firebase UID
+    suspend fun getUserByFirebaseUid(
+        firebaseUid: String
+    ): UserEntity? {
+        return userDao.getUserByFirebaseUid(firebaseUid)
+    }
+
+    suspend fun getAllUsers(): List<UserEntity> {
+        return userDao.getAllUsers()
+    }
+
+    // UPDATE
+    suspend fun updateUser(
+        user: UserEntity
+    ) {
+        userDao.updateUser(user)
+    }
+
+    // DELETE
+    suspend fun deleteUser(
+        user: UserEntity
+    ) {
+        userDao.deleteUser(user)
+    }
+}
