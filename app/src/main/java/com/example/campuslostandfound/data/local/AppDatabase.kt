dce4c00e4882fa24886cff7a16d0fff8d3b2f686
Martina <1peresmartina@gmail.com>
2026-08-10 11:19:42 +0300

    Update AppDatabase.kt

--- app/src/main/java/com/example/campuslostandfound/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/campuslostandfound/data/local/AppDatabase.kt
@@ -1,2 +1,19 @@
 package com.example.campuslostandfound.data.local
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [
+        UserEntity::class,
+        LostFoundItemEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun userDao(): UserDao
+
+    abstract fun lostFoundItemDao(): LostFoundItemDao
+}
