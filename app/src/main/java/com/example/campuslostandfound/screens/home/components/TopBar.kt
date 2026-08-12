238500f276b30cfdfbf6e029d8ade667bf709a16
Martina <1peresmartina@gmail.com>
2026-08-12 08:46:06 +0300

    Update TopBar.kt

--- app/src/main/java/com/example/campuslostandfound/screens/home/components/TopBar.kt
+++ app/src/main/java/com/example/campuslostandfound/screens/home/components/TopBar.kt
@@ -1,7 +1,11 @@
 package com.example.campuslostandfound.screens.home.components
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -9,16 +13,32 @@ import com.example.campuslostandfound.ui.theme.PrimaryGreen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBar() {
+fun TopBar(
+    onLogout: () -> Unit
+) {
 
     CenterAlignedTopAppBar(
+
         title = {
             Text(
                 text = "Campus Lost & Found",
                 style = MaterialTheme.typography.titleLarge,
                 color = PrimaryGreen
             )
+        },
+
+        actions = {
+
+            IconButton(
+                onClick = onLogout
+            ) {
+
+                Icon(
+                    imageVector = Icons.Default.ExitToApp,
+                    contentDescription = "Logout",
+                    tint = PrimaryGreen
+                )
+            }
         }
     )
-
 }
